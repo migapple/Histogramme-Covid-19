e5c7f24a3d17db8dcc7e00872aa169ed1cf5505b
Michel Garlandat <michel.garlandat@me.com>
2020-04-04 17:38:44 +0200

    Initial Commit

---
+++ Histogramme Covid 19/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Histogramme Covid 19
+//
+//  Created by Michel Garlandat on 04/04/2020.
+//  Copyright © 2020 Michel Garlandat. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
